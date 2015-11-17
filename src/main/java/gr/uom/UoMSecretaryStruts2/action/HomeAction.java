ab4fc8b8741182e6e1c6b5e9708167ebabc37ee3
Georgios Digkas <ma-los-sis@hotmail.com>
2015-11-17 14:59:11 +0200

    Home Action class for handling home url requests created

---
+++ src/main/java/gr/uom/UoMSecretaryStruts2/action/HomeAction.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package gr.uom.UoMSecretaryStruts2.action;
+
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import gr.uom.UoMSecretaryStruts2.domain.Lesson;
+import gr.uom.UoMSecretaryStruts2.domain.UserDetails;
+import gr.uom.UoMSecretaryStruts2.service.LessonService;
+import gr.uom.UoMSecretaryStruts2.service.UserDetailsService;
+
+/**
+ * @author Georgios Digkas <mai153@uom.edu.gr>
+ *
+ */
+public class HomeAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+
+	private LessonService lessonService;
+	private List<Lesson> lessons;
+
+	private UserDetailsService userDetailsService;
+	private List<UserDetails> professors;
+	private List<UserDetails> secretaries;
+	private UserDetails userDetails;
+
+	public String lessons() throws Exception {
+		this.setLessons(lessonService.findAll());
+		return SUCCESS;
+	}
+
+	public String professors() throws Exception {
+		this.setProfessors(userDetailsService.findByRole("ROLE_PROFESSOR"));
+		return SUCCESS;
+	}
+
+	public String secretaries() throws Exception {
+		this.setSecretaries(userDetailsService.findByRole("ROLE_SECRETARY"));
+		return SUCCESS;
+	}
+
+	public List<UserDetails> getProfessors() {
+		return professors;
+	}
+	public void setProfessors(List<UserDetails> professors) {
+		this.professors = professors;
+	}
+	public void setUserDetailsService(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+	public List<Lesson> getLessons() {
+		return lessons;
+	}
+	public void setLessons(List<Lesson> lessons) {
+		this.lessons = lessons;
+	}
+	public void setLessonService(LessonService lessonService) {
+		this.lessonService = lessonService;
+	}
+	public List<UserDetails> getSecretaries() {
+		return secretaries;
+	}
+	public void setSecretaries(List<UserDetails> secretaries) {
+		this.secretaries = secretaries;
+	}
+	public UserDetails getUserDetails() {
+		return userDetails;
+	}
+	public void setUserDetails(UserDetails userDetails) {
+		this.userDetails = userDetails;
+	}
+
+}
