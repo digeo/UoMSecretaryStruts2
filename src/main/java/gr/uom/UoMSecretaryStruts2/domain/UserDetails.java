adc8abbd16c90e4863d8ffd932aca947e637c8d5
Georgios Digkas <ma-los-sis@hotmail.com>
2015-12-26 17:13:45 +0200

    The associated tasks to TODO comment, completed (Info Issues)

--- src/main/java/gr/uom/UoMSecretaryStruts2/domain/UserDetails.java
+++ src/main/java/gr/uom/UoMSecretaryStruts2/domain/UserDetails.java
@@ -28,134 +28,134 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Table(name = "user_details")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "UserDetails.findAll", query = "SELECT u FROM UserDetails u"),
-    @NamedQuery(name = "UserDetails.findByUsername", query = "SELECT u FROM UserDetails u WHERE u.username = :username"),
-    @NamedQuery(name = "UserDetails.findByName", query = "SELECT u FROM UserDetails u WHERE u.name = :name"),
-    @NamedQuery(name = "UserDetails.findBySurname", query = "SELECT u FROM UserDetails u WHERE u.surname = :surname"),
-    @NamedQuery(name = "UserDetails.findBySex", query = "SELECT u FROM UserDetails u WHERE u.sex = :sex"),
-    @NamedQuery(name = "UserDetails.findByTelephone", query = "SELECT u FROM UserDetails u WHERE u.telephone = :telephone"),
-    @NamedQuery(name = "UserDetails.findByAddress", query = "SELECT u FROM UserDetails u WHERE u.address = :address"),
-    @NamedQuery(name = "UserDetails.findByBirthday", query = "SELECT u FROM UserDetails u WHERE u.birthday = :birthday"),
-    @NamedQuery(name = "UserDetails.findByRole", query = "SELECT u FROM UserDetails u WHERE u.username IN (SELECT user.username FROM User user WHERE user.role = :role)")})
+	@NamedQuery(name = "UserDetails.findAll", query = "SELECT u FROM UserDetails u"),
+	@NamedQuery(name = "UserDetails.findByUsername", query = "SELECT u FROM UserDetails u WHERE u.username = :username"),
+	@NamedQuery(name = "UserDetails.findByName", query = "SELECT u FROM UserDetails u WHERE u.name = :name"),
+	@NamedQuery(name = "UserDetails.findBySurname", query = "SELECT u FROM UserDetails u WHERE u.surname = :surname"),
+	@NamedQuery(name = "UserDetails.findBySex", query = "SELECT u FROM UserDetails u WHERE u.sex = :sex"),
+	@NamedQuery(name = "UserDetails.findByTelephone", query = "SELECT u FROM UserDetails u WHERE u.telephone = :telephone"),
+	@NamedQuery(name = "UserDetails.findByAddress", query = "SELECT u FROM UserDetails u WHERE u.address = :address"),
+	@NamedQuery(name = "UserDetails.findByBirthday", query = "SELECT u FROM UserDetails u WHERE u.birthday = :birthday"),
+	@NamedQuery(name = "UserDetails.findByRole", query = "SELECT u FROM UserDetails u WHERE u.username IN (SELECT user.username FROM User user WHERE user.role = :role)")})
 public class UserDetails implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @Column(name = "username")
-    private String username;
-    @Column(name = "name")
-    private String name;
-    @Column(name = "surname")
-    private String surname;
-    @Column(name = "sex")
-    private String sex;
-    @Column(name = "telephone")
-    private String telephone;
-    @Column(name = "address")
-    private String address;
-    @Column(name = "birthday")
-    @Temporal(TemporalType.DATE)
-    private Date birthday;
-    @JoinColumn(name = "username", referencedColumnName = "username", insertable = false, updatable = false)
-    @OneToOne(optional = false)
-    private User user;
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Basic(optional = false)
+	@Column(name = "username")
+	private String username;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "surname")
+	private String surname;
+	@Column(name = "sex")
+	private String sex;
+	@Column(name = "telephone")
+	private String telephone;
+	@Column(name = "address")
+	private String address;
+	@Column(name = "birthday")
+	@Temporal(TemporalType.DATE)
+	private Date birthday;
+	@JoinColumn(name = "username", referencedColumnName = "username", insertable = false, updatable = false)
+	@OneToOne(optional = false)
+	private User user;
 
-    public UserDetails() {
-    	//Empty constructor for setting values
-    }
+	public UserDetails() {
+		//Empty constructor for setting values
+	}
 
-    public UserDetails(String username) {
-        this.username = username;
-    }
+	public UserDetails(String username) {
+		this.username = username;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getSurname() {
-        return surname;
-    }
+	public String getSurname() {
+		return surname;
+	}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-    public String getSex() {
-        return sex;
-    }
+	public String getSex() {
+		return sex;
+	}
 
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
 
-    public String getTelephone() {
-        return telephone;
-    }
+	public String getTelephone() {
+		return telephone;
+	}
 
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    public Date getBirthday() {
-        return birthday;
-    }
+	public Date getBirthday() {
+		return birthday;
+	}
 
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	public User getUser() {
+		return user;
+	}
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (username != null ? username.hashCode() : 0);
-        return hash;
-    }
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (username != null ? username.hashCode() : 0);
+		return hash;
+	}
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof UserDetails)) {
-            return false;
-        }
-        UserDetails other = (UserDetails) object;
-        if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object object) {
+		//Warning - this method won't work in the case the id fields are not set
+		if (!(object instanceof UserDetails)) {
+			return false;
+		}
+		UserDetails other = (UserDetails) object;
+		if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
+			return false;
+		}
+		return true;
+	}
 
-    @Override
-    public String toString() {
-        return "gr.uom.uomsecretarystruts2.domain.UserDetails[ username=" + username + " ]";
-    }
-    
+	@Override
+	public String toString() {
+		return "gr.uom.uomsecretarystruts2.domain.UserDetails[ username=" + username + " ]";
+	}
+
 }
